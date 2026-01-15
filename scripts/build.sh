9251961ba1928465b288c71dd842631b0bf12749
M0hameed0 <mohamed.n-ait-boufous@supdevinci-edu.fr>
2026-01-15 10:21:13 +0100

    ajout pilpline build et deploy

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Étudiant B & C - Build de l'image
+IMAGE_NAME="ghcr.io/${GITHUB_REPOSITORY_OWNER,,}/api-conduit"
+
+echo " Building Docker image..."
+# Construction avec le SHA pour la traçabilité et latest pour la prod
+docker build -t $IMAGE_NAME:latest -t $IMAGE_NAME:$GITHUB_SHA -f docker/Dockerfile .
