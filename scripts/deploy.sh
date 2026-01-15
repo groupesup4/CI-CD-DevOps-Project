df96d0e9d0fcbd90ed9b76a935c7e8b82c9dbed4
M0hameed0 <mohamed.n-ait-boufous@supdevinci-edu.fr>
2026-01-15 12:16:56 +0100

    modif deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-echo "--------------------------------------------------------"
-echo "🚀 Rapport de déploiement pour : $AZURE_WEBAPP_NAME"
-echo "📦 Image déployée : ghcr.io/${GITHUB_REPOSITORY_OWNER,,}/api-nodejs:latest"
-echo "✅ Statut : Déploiement validé via Publish Profile"
-echo "🔗 URL : https://$AZURE_WEBAPP_NAME.azurewebsites.net"
-echo "--------------------------------------------------------"
+# Étudiant C - Connexion sécurisée Registry <-> Azure
+
+echo "Configuration des accès au registre pour Azure..."
+
+# Cette commande dit à Azure comment s'identifier sur GHCR
+az webapp config container set \
+  --name "$AZURE_WEBAPP_NAME" \
+  --resource-group "rg-groupesup4-api" \
+  --docker-custom-image-name "ghcr.io/${GITHUB_REPOSITORY_OWNER,,}/api-nodejs:latest" \
+  --docker-registry-server-url "https://ghcr.io" \
+  --docker-registry-server-user "$GITHUB_ACTOR" \
+  --docker-registry-server-password "$GITHUB_TOKEN"
+
+echo "Image mise à jour et accès autorisé !"
