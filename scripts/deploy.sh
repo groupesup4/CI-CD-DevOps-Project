e327245f4239eca63261dd995c2b72d6ac6f264d
M0hameed0 <mohamed.n-ait-boufous@supdevinci-edu.fr>
2026-01-15 11:51:28 +0100

    test deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
-# Étudiant C - CD & SecOps
+echo " Mise à jour de l'image sur Azure App Service..."
 
-# On s'assure que le nom est en minuscules
-IMAGE_NAME="ghcr.io/${GITHUB_REPOSITORY_OWNER,,}/api-nodejs:latest"
-
-echo "🚀 Démarrage du déploiement sur Azure Container Instances..."
-
-az container create \
-  --resource-group "$AZURE_RESOURCE_GROUP" \
-  --name app-nodejs \
-  --image "$IMAGE_NAME" \
-  --cpu 1 \
-  --memory 1.5 \
-  --registry-login-server ghcr.io \
-  --registry-username "$GITHUB_ACTOR" \
-  --registry-password "$GITHUB_TOKEN" \
-  --dns-name-label "conduit-api-group4" \
-  --ports 3000 \
-  --location "West Europe" \
-  --environment-variables DATABASE_URL="$DATABASE_URL" \
-  --restart-policy OnFailure
-
-echo "✅ Déploiement terminé avec succès !"
+# Mise à jour de la configuration container
+az webapp config container set \
+  --name "$AZURE_WEBAPP_NAME" \
+  --resource-group "rg-groupesup4-api" \
+  --container-image-name "ghcr.io/${GITHUB_REPOSITORY_OWNER,,}/api-nodejs:latest" \
+  --container-registry-url "https://ghcr.io" \
+  --container-registry-user "$GITHUB_ACTOR" \
+  --container-registry-password "$GITHUB_TOKEN"
